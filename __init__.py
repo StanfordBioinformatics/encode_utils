6479cdc9a1b9203d8e5f327c39344d00b68bd326
Nathaniel Watson <nathankw@stanford.edu>
2018-01-12 13:20:02 -0800

    removed api keys conf file references in init file.

--- __init__.py
+++ __init__.py
@@ -12,26 +12,6 @@ DCC_AWARD_ATTR = "award"
 DCC_LAB_ATTR = "lab"
 AWARDLESS_PROFILES = ["replicate"] #these profiles don't have the 'award' and 'lab' attributes.
 
-
-#custom variables
-DCC_API_KEYS_FILE = os.path.join(package_path,"dcc_submitters.json")
-#The file pointed to by DCC_API_KEYS_FILE should have the following JSON structure:
-#
-#{
-#  "usr1": {
-#    "email": "usr1@stanford.edu",
-#    "api_key": "######",
-#    "secret_key": "###########"
-#  },  
-#
-#  "usr2": {   
-#    "email": "usr2@stanford.edu",
-#    "api_key": "######",
-#    "secret_key": "###########"
-#  }
-#
-#}
-
 LAB = conf["lab"]
 LAB_PREFIX = LAB + ":" 
 AWARD = conf["award"] 
