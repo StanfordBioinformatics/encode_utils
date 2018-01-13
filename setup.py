84519193f53b56d133022bdcb711ea9b0f6f4c08
Nathaniel Watson <nathankw@stanford.edu>
2018-01-12 16:13:51 -0800

    Fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
 	url = "https://github.com/StanfordBioinformatics/encode_utils/wiki",
 	packages =["encode_utils"],
 	scripts = ["encode_utils/MetaDataRegistration/register_meta.py"],
-	data_files = [ ("encode_utils",["encode_utils/conf_data.json"]) ]
+	package_data = {"encode_utils": ["conf_data.json"]}
 )
