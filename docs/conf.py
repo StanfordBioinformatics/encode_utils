d9fb8b65bb2c48ce3023c184c067233f78ebd6b7
Nathaniel Watson <nathankw@DN0a22b262.SUNet>
2018-02-09 15:23:18 -0800

    Added it back

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.githubpages',
+    'sphinxarg.ext'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
