474fb5489c90b7b96360acf2a5395e8970d9b70e
Nathaniel Watson <nathankw@stanford.edu>
2018-03-20 12:53:51 -0700

    Updated path to data directory so that tests can be run outside of the tests folder.

---
+++ encode_utils/tests/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-                                                                                
+                                                                                                       
+###                                                                                                    
+# © 2018 The Board of Trustees of the Leland Stanford Junior University                                
+# Nathaniel Watson                                                                                     
+# nathankw@stanford.edu                                                                                
+###
+
+import os
+
+DATA_DIR = os.path.join(os.path.dirname(__file__),"data")
