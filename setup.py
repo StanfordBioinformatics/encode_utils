f7e680fa2a84d065a8d1af8e144a4f88b48e475e
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-11-02 01:29:28 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ scripts.append("encode_utils/MetaDataRegistration/eu_register.py")
 
 setup(
   name = "encode utils",
-  version = "2.4.0",
+  version = "2.5.0",
   description = "Client and tools for ENCODE data submitters.",
   author = "Nathaniel Watson",
   author_email = "nathankw@stanford.edu",
